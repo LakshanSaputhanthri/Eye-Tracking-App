d0c5f3103f33016748de491ed762dd794489c00e
LakshanSaputhanthri <lakshan.sv25@gmail.com>
2024-03-14 19:07:00 +0530

    Setup android notifications

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import LoginForm from "./component/LoginForm";
+import { LogLevel, OneSignal } from "react-native-onesignal";
+import "react-native-gesture-handler";
+
+const initOneSignal = () => {
+  OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+  OneSignal.initialize("a2533e4a-e8de-4d2d-a612-9c70fb496e50");
+  OneSignal.Notifications.requestPermission(true);
+};
 
 const Index = () => {
+  initOneSignal();
+
   return (
     <View style={styles.container}>
       <LoginForm />
